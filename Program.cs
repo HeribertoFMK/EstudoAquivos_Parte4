5e0df3fcb9e2f6f424cb7891a79d033da775c106
Heriberto FMK <hfmk2015@gmail.com>
2021-08-16 10:52:24 +0900

    Finalizado o estudo sobre StreamWriter

--- Program.cs
+++ Program.cs
@@ -10,6 +10,33 @@ namespace SobreStreamWriter
         static void Main(string[] args)
         {
 
+            string SourcePath = @"C:\csharp\texto1.txt";
+            string TargetPathh = @"C:\csharp\texto2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(SourcePath);
+                using (StreamWriter sw = File.AppendText(TargetPathh))
+                {
+
+                    foreach (string line in lines)
+                    {
+
+                        sw.WriteLine(line.ToUpper());
+
+                    }
+
+
+
+                }
+            }
+            catch (IOException e)
+            {
+                System.Console.WriteLine("An Error Occurred!");
+                System.Console.WriteLine(e.Message);
+            }
+
+
 
 
 
