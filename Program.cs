2d2c08b0090d9beb0f09f3912a64c9cebcff4bf7
Heriberto FMK <hfmk2015@gmail.com>
2021-08-16 10:32:31 +0900

    Inicio do estudos sobre StreamWriter

--- Program.cs
+++ Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Globalization;
-using exerciciofinaltryecatch.Entities;
-using exerciciofinaltryecatch.Entities.Exceptions;
+using System.IO;
 
 
 
-
-namespace exerciciofinaltryecatch
+namespace SobreStreamWriter
 {
     class Program
     {
@@ -14,39 +11,8 @@ namespace exerciciofinaltryecatch
         {
 
 
-           
-
-            Console.WriteLine("Enter account data");
-            System.Console.WriteLine();
-            System.Console.Write("Number: ");
-            int number = int.Parse(Console.ReadLine());            
-            System.Console.Write("Holder: ");
-            string holder = Console.ReadLine();            
-            System.Console.Write("Initial Balance: ");
-            double balance = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
-            System.Console.Write("Width Limit: ");
-            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Account account = new Account(number,holder,balance,withdrawLimit);
-            System.Console.Write("Enter amount for withdraw: ");
-            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            try{
-
-           
-            account.Withdraw(amount);
-
-            System.Console.WriteLine("new Balance:  " + account.Balance.ToString("F2",CultureInfo.InvariantCulture));
-
-            }
-         
-            catch(DomainException e){
 
 
-                System.Console.WriteLine("Saldo Insuficiente para saque" +e.Message);
-
-            }
-            
 
         }
     }
